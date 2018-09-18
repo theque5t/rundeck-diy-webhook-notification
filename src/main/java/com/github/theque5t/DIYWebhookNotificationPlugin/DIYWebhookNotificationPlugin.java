ed2753c6b48318c75c0a438724d588c03c0bb2db
unknown <theque5t@users.noreply.github.com>
2018-09-17 23:14:57 -0500

    Refactored: variable names, execution data lookup, error handling, message body property description, dependencies, templating logic

--- src/main/java/com/github/theque5t/DIYWebhookNotificationPlugin/DIYWebhookNotificationPlugin.java
+++ src/main/java/com/github/theque5t/DIYWebhookNotificationPlugin/DIYWebhookNotificationPlugin.java
@@ -1,5 +1,6 @@
 package com.github.theque5t.DIYWebhookNotificationPlugin;
 
+import liqp.Template;
 import com.dtolabs.rundeck.core.plugins.Plugin;
 import com.dtolabs.rundeck.plugins.notification.NotificationPlugin;
 import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
@@ -10,14 +11,13 @@ import static com.dtolabs.rundeck.core.plugins.configuration.StringRenderingCons
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 @Plugin(service="Notification",name="DIYWebhookNotificationPlugin")
-@PluginDescription(title="DIY Webhook Notification Plugin", description="The DIY(do it yourself) webhook notification plugin that lets you supply your own custom messages.")
+@PluginDescription(title="DIY Webhook Notification Plugin", description="The DIY(do it yourself) webhook notification plugin that lets you supply your own custom messages.\n___\nProject lives [here](https://github.com/theque5t/rundeck-diy-webhook-notification)\n___\n")
 public class DIYWebhookNotificationPlugin implements NotificationPlugin{
 	
 	@PluginProperty(
@@ -39,19 +39,40 @@ public class DIYWebhookNotificationPlugin implements NotificationPlugin{
     		name = "messageBody",
     		title = "Message Body",
     		description = "The message body. Example: {\"text\":\"Hello world!\"}  \n"
-    					+ "You can add execution data to your message following this syntax: `$<map>.<key>$`  \n"
-    					+ "Example: `{\"text\":\"Job $job.name$ has $execution.status$\"}`  \n"
-    					+ "Valid map values are:\n"
-    					+ " - `execution`\n"
-    					+ " - `job`\n"
-    					+ " - `nodeStatus`\n"
-    					+ " - `globalContext`\n"
-    					+ " - `jobContext`\n"
-    					+ " - `jobOption`\n"
+    					+ "___  \n"
+    					+ "#### Building the Message  \n"
+    					+ "The execution order and summary of what occurs when building the final message body is as follows:  \n" 
+    					+ " 1. Any embedded property references will be replaced with the runtime value.  \n"
+    					+ " 2. Any execution data references will be replaced with the runtime value.  \n"
+    					+ " 3. Any template markup will be rendered.  \n"
+    					+ "___  \n"
+    					+ "#### Embedded Property References  \n"
+    					+ "You can add [embedded property references](https://rundeck.org/docs/developer/notification-plugin.html) to your message following this syntax: `${group.key}`  \n"
     					+ "  \n"
-    					+ "Valid key values are:\n"
-    					+ " - execution, job, and nodeStatus keys: see [https://rundeck.org/docs/developer/notification-plugin.html](https://rundeck.org/docs/developer/notification-plugin.html)\n"
-    					+ " - globalContext, jobContext, and jobOption keys: see [https://rundeck.org/docs/manual/creating-job-workflows.html#context-variables](https://rundeck.org/docs/manual/creating-job-workflows.html#context-variables)\n",
+    					+ "Example for \"On Start\": `{\"text\":\"Job ${job.name}(#${job.execid}): Started\"}`  \n"
+    					+ "___  \n"
+    					+ "#### Execution Data References  \n"
+    					+ "You can add [execution data references](https://rundeck.org/docs/developer/notification-plugin.html) to your message following this syntax: `$map.key$`  \n"
+    					+ "  \n"
+    					+ "Examples:  \n"
+    					+ " - `{\"text\":\"Job ${job.name}(#${job.execid}): $execution.status$\"}`  \n"
+    					+ " - `{\"text\":\"Job ${job.name}(#$execution.id$): $execution.status$\"}`  \n"
+    					+ " - `{\"text\":\"Job ${job.name}(#${job.execid}) from Group $execution.context.job.group$: $execution.status$\"}`  \n"
+    					+ "  \n"
+    					+ "[Refer here](https://rundeck.org/docs/developer/notification-plugin.html#execution-data) and [here](https://rundeck.org/docs/manual/creating-job-workflows.html#context-variables) to see what data is available.  \n"
+    					+ "You can also supply a reference that equals an entire map. For example:  \n"
+    					+ "```\n"
+    					+ "{\"text\":\"Job: ${job.name} \nId: $execution.job.id$\nStatus: $execution.status$\nJob Details: $execution.job$\"}\n"
+    					+ "```\n"
+    					+ "  \n"
+    					+ "___  \n"
+    					+ "#### Template Markup  \n"
+    					+ "You can add template markup following [the template language](https://shopify.github.io/liquid/) syntax.  \n"
+    					+ "  \n"
+    					+ "Example: `{\"text\":\"Job ${job.name}(#${job.execid}): {{ \"$execution.status$\" | capitalize }}\"}`  \n"
+    					+ "  \n"
+    					+ "Refer to the [documentation for designers](https://github.com/Shopify/liquid/wiki/Liquid-for-Designers) for further assistance with the template language.  \n"
+    					+ "___  \n",
 			required = true)
     @TextArea
     private String messageBody;
@@ -60,91 +81,77 @@ public class DIYWebhookNotificationPlugin implements NotificationPlugin{
 
     }
 
-    public class CustomMessageException extends Exception {
-  	  public CustomMessageException(String message){
+    private class CustomMessageException extends Exception {
+  	  private CustomMessageException(String message){
   		     super(message);
   	  }
   	}
     
-	private String formatMessage(String theMessage, Map data) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, CustomMessageException {
+	private String formatMessage(String theMessageBody, Map theExecutionData) {
 		
-		Pattern pattern = Pattern.compile("(\\$)(.*?)(\\$)");
-        Matcher matcher = pattern.matcher(theMessage);
-        StringBuffer buffer = new StringBuffer(theMessage.length());
-        while(matcher.find())
+		Pattern executionDataReferencePattern = Pattern.compile("\\$([a-z].*?[a-z])\\$");
+        Matcher executionDataReferenceMatches = executionDataReferencePattern.matcher(theMessageBody);
+        StringBuffer theMessageBodyWithExecutionDataBuffer = new StringBuffer(theMessageBody.length());
+        
+        while(executionDataReferenceMatches.find())
         {
-        	String mapName = matcher.group(2).substring(0, matcher.group(2).indexOf("."));
-        	String keyName = matcher.group(2).substring(matcher.group(2).indexOf(".")+1, matcher.group(2).length());
-        	        	
-        	Map theMap = null;
-        	boolean processMap = true;
-        	if(mapName.equals("execution"))
-        	{
-        		theMap = (Map) data;
-        	} 
-        	else if(mapName.equals("job")) 
-        	{
-        		theMap = (Map) data.get("job");
-        	} 
-        	else if(mapName.equals("nodeStatus")) 
-        	{
-        		theMap = (Map) data.get("nodestatus");        		
-        	} 
-        	else if(mapName.equals("globalContext")) 
-        	{
-        		Map theContext = (Map) data.get("context");
-        		theMap = (Map) theContext.get("globals");
-        	} 
-        	else if (mapName.equals("jobContext")) 
-        	{
-        		Map theContext = (Map) data.get("context");
-        		theMap = (Map) theContext.get("job");        		
-        	} 
-        	else if (mapName.equals("jobOption")) 
-        	{
-        		Map theContext = (Map) data.get("context");
-        		theMap = (Map) theContext.get("option");
-        	} 
-        	else 
-        	{
-        		throw new CustomMessageException(mapName+" is not a valid map");
-        	}
-        	
-        	
-        	String theValue = null;
-        	if (processMap) 
-        	{
-        		if (theMap.containsKey(keyName)) 
-        		{
-            		theValue = (String) theMap.get(keyName);        			
-        		} 
-        		else 
-        		{
-        			throw new CustomMessageException(keyName+" is not a valid key for map "+mapName);
-        		}
-        	}
-        	
-        	matcher.appendReplacement(buffer, theValue);        	
+        	String executionDataReferenceMatch = executionDataReferenceMatches.group(1);
+	        String[] executionDataReferenceCommand = executionDataReferenceMatch.split("\\.");
+	        Map theCurrentMap = theExecutionData;
+	        
+	        int i = 0;
+	        do {
+	        	String theCurrentKey = executionDataReferenceCommand[i];
+	        	i++;
+	        	
+	        	if(theCurrentMap.containsKey(theCurrentKey) && i < executionDataReferenceCommand.length)
+	        	{
+	        		theCurrentMap = (Map) theCurrentMap.get(theCurrentKey);
+	        	}
+	        	else if (theCurrentMap.containsKey(theCurrentKey))
+	        	{	        		
+	        		executionDataReferenceMatches.appendReplacement(theMessageBodyWithExecutionDataBuffer, theCurrentMap.get(theCurrentKey)== null ? "" : theCurrentMap.get(theCurrentKey).toString());
+	        	}
+	        	else if (theCurrentKey.equals("execution") && executionDataReferenceCommand.length == 1)
+	        	{
+	        		executionDataReferenceMatches.appendReplacement(theMessageBodyWithExecutionDataBuffer, theCurrentMap.toString());
+	        	}
+	        	else if (!theCurrentKey.equals("execution"))
+	        	{
+	        		executionDataReferenceMatches.appendReplacement(theMessageBodyWithExecutionDataBuffer, "InvalidReference-->`"+executionDataReferenceMatch+"`<--InvalidReference");
+	        		break;
+	        	}
+	    	}
+	        while(i < executionDataReferenceCommand.length);
         }
-        matcher.appendTail(buffer);
-        String theNewMessage = buffer.toString();
+        
+        executionDataReferenceMatches.appendTail(theMessageBodyWithExecutionDataBuffer);
+        String theMessageBodyWithExecutionData = theMessageBodyWithExecutionDataBuffer.toString();
+        
+        Template theMessageBodyAsTemplate = Template.parse(theMessageBodyWithExecutionData);
+		String theNewMessage = theMessageBodyAsTemplate.render();
+        
         return theNewMessage;
 	}
     
-	private String sendMessage(String endpoint, String contentTypeHeader, String content) throws IOException, CustomMessageException {
-		URL url = new URL(endpoint);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setConnectTimeout(5000);
-		connection.setReadTimeout(5000);
-		connection.setRequestMethod("POST");
-		connection.setRequestProperty("Content-type", contentTypeHeader);
-		connection.setDoOutput(true);
-		DataOutputStream body = new DataOutputStream(connection.getOutputStream());
-		body.write(content.getBytes("UTF-8"));
-		body.flush();
-		body.close();
-		int responseCode = connection.getResponseCode();
-		connection.disconnect();
+	private String sendMessage(String theWebhookUrl, String theContentType, String theFormattedMessage) throws IOException, CustomMessageException {
+		
+		HttpURLConnection connectionToWebhook = (HttpURLConnection) new URL(theWebhookUrl).openConnection();
+		
+		connectionToWebhook.setConnectTimeout(5000);
+		connectionToWebhook.setReadTimeout(5000);
+		connectionToWebhook.setRequestMethod("POST");
+		connectionToWebhook.setRequestProperty("Content-type", theContentType);
+		connectionToWebhook.setDoOutput(true);
+		
+		DataOutputStream bodyOfRequest = new DataOutputStream(connectionToWebhook.getOutputStream());
+		bodyOfRequest.write(theFormattedMessage.getBytes("UTF-8"));
+		bodyOfRequest.flush();
+		bodyOfRequest.close();
+		
+		int responseCode = connectionToWebhook.getResponseCode();
+		connectionToWebhook.disconnect();
+		
 		String result = "The response code is: "+responseCode;
 		
 		if(responseCode != 200)
@@ -156,31 +163,17 @@ public class DIYWebhookNotificationPlugin implements NotificationPlugin{
 	}
 	    
     public boolean postNotification(String trigger, Map executionData, Map config){
-    	String formattedMessage = null;
-    	String result = null;
+    	
     	try
     	{
-    		formattedMessage = formatMessage(messageBody,executionData);	
-			result = sendMessage(webhookUrl,contentType,formattedMessage);
+    		String formattedMessage = formatMessage(messageBody,executionData);	
+    		sendMessage(webhookUrl,contentType,formattedMessage);
     	}
-		catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException | CustomMessageException | IOException e)
+		catch ( SecurityException | IllegalArgumentException | CustomMessageException | IOException e)
 		{
-			Map<String, Object> pluginData = new HashMap();
-    		pluginData.put("trigger", trigger);
-    		pluginData.put("executionData", executionData);
-    		pluginData.put("config", config);
-    		pluginData.put("webhookUrl", webhookUrl);
-    		pluginData.put("contentType", contentType);
-    		pluginData.put("messageBody", messageBody);
-    		pluginData.put("formattedMessage", formattedMessage);
-    		pluginData.put("result", result);
-    		for (Map.Entry<String, Object> entry : pluginData.entrySet())
-    		{
-    		    System.err.println("Name:  '" + entry.getKey() + "'");
-    		    System.err.println("Value: '" + entry.getValue() + "'");
-    		}
 			e.printStackTrace();
 		}
-        return true;
+        
+    	return true;
     }
 }
